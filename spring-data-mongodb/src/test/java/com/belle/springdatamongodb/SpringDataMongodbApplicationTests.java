b26e447e34b68505b39e57caa3b25a9878187888
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-07-01 19:12:28 -0600

    Adding module for Spring Data MongoDB.

---
+++ spring-data-mongodb/src/test/java/com/belle/springdatamongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belle.springdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
