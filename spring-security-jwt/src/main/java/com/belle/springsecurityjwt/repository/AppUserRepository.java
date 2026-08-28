f88c6e09095dfed52c2df1bc181835cf2e69bf82
Mildred Zaragoza <mildredzaragozav@gmail.com>
2026-08-28 12:01:01 -0600

    Handling optional/null users in UserDetailServiceImpl (CustomUserDetailsService).

--- spring-security-jwt/src/main/java/com/belle/springsecurityjwt/repository/AppUserRepository.java
+++ spring-security-jwt/src/main/java/com/belle/springsecurityjwt/repository/AppUserRepository.java
@@ -4,7 +4,9 @@ import com.belle.springsecurityjwt.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
-    AppUser findByUsername(String username);
+    Optional<AppUser> findByUsername(String username);
 }
--- spring-security-jwt/src/main/java/com/belle/springsecurityjwt/service/CustomUserDetailsService.java
+++ spring-security-jwt/src/main/java/com/belle/springsecurityjwt/service/CustomUserDetailsService.java
@@ -3,11 +3,16 @@ package com.belle.springsecurityjwt.service;
 import com.belle.springsecurityjwt.model.AppUser;
 import com.belle.springsecurityjwt.repository.AppUserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Service
@@ -18,15 +23,18 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         @Override
         public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-            AppUser customUser = appUserRepository.findByUsername(username);
-
-            if (customUser == null) {
-                throw new UsernameNotFoundException("User '" + username + "' not found.");
-            }
+            AppUser appUser = appUserRepository
+                    .findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found."));
 
             return User
-                    .withUsername(customUser.getUsername())
-                    .password(customUser.getPassword())
+                    .withUsername(appUser.getUsername())
+                    .password(appUser.getPassword())
+                    .authorities(getAuthorities(appUser))
                     .build();
+        }
+
+        private Collection<? extends GrantedAuthority> getAuthorities(AppUser appUser) {
+            return List.of(new SimpleGrantedAuthority(appUser.getAuthority()));
         }
 }
